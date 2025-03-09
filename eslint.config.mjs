55abd973966fd822e6f833d9ea83477b3e00bed6
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-03-09 06:50:55 -0300

    i go bed now

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +9,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      'quotes': ['error', 'single'],
+      'jsx-quotes': ['error', 'prefer-single'],
+      'import/quotes': ['error', 'single'],
+    },
+  },
 ];
 
 export default eslintConfig;
